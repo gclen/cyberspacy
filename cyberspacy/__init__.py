88de72e958da6e58b33ec6bbca1ffab74945b16e
Clendenning, Graham P <gpclend@cse-cst.gc.ca>
2019-02-08 11:08:04 -0500

    Refactor into separate classes

--- cyberspacy/__init__.py
+++ cyberspacy/__init__.py
@@ -7,16 +7,14 @@ from spacy.tokens import Doc, Span, Token
 from spacy.matcher import Matcher
 
 from .about import __version__
-from .expressions import ipv4_expr
+from .expressions import ipv4_expr, url_expr, email_expr
 
 
-class Cyber(object):
-    """spaCy v2.0 pipeline component for adding meta data about IPs, domains and
-     email addresses to `Doc` objects.
-    """
-    name='cyberspacy'
+class IPDetector(object):
+    """spaCy v2.0 pipeline component for adding IP meta data to `Doc` objects."""
+    name='ip_detection'
 
-    def __init__(self, nlp, pattern_id='CYBER', attrs=('has_ipv4', 'is_ipv4', 'ipv4')):
+    def __init__(self, nlp, pattern_id='IPDetector', attrs=('has_ipv4', 'is_ipv4', 'ipv4')):
         self._has_ipv4, self._is_ipv4, self._ipv4 = attrs
         self.matcher = Matcher(nlp.vocab)
 
@@ -49,10 +47,97 @@ class Cyber(object):
 
         return doc
 
-
     def has_ipv4(self, tokens):
         return any(token._.get(self._is_ipv4) for token in tokens)
 
     def iter_ipv4(self, tokens):
         return [(i, t) for i, t in enumerate(tokens) if t._.get(self._is_ipv4)]        
+
+
+class URLDetector(object):
+    """spaCy v2.0 pipeline component for adding URL meta data to `Doc` objects."""
+    name='url_detection'
+
+    def __init__(self, nlp, pattern_id='URLDetector', attrs=('has_url', 'is_url', 'url')):
+        self._has_url, self._is_url, self._url = attrs
+        self.matcher = Matcher(nlp.vocab)
+
+        # Add  URL rule to matcher
+        self._url_re = re.compile(url_expr, re.VERBOSE | re.I | re.UNICODE)
+        url_mask = lambda text: bool(self._url_re.match(text))
+        url_flag = nlp.vocab.add_flag(url_mask)
+        self.matcher.add('url', None, [{url_flag: True}])
+        
+        # Add attributes
+        Doc.set_extension(self._has_url, getter=self.has_url)
+        Doc.set_extension(self._url, getter=self.iter_url)
+        Span.set_extension(self._has_url, getter=self.has_url)
+        Span.set_extension(self._url, getter=self.iter_url)
+        Token.set_extension(self._is_url, default=False)
+
+    def __call__(self, doc):
+        """Apply the pipeline component to a `Doc` object.
+
+        doc (Doc): The `Doc` returned by the previous pipeline component.
+        RETURNS (Doc): The modified `Doc` object.
+        """
+        matches = self.matcher(doc)
+        spans = []  # keep spans here to merge them later
+        for match_id, start, end in matches:
+            span = doc[start : end]
+            for token in span:
+                token._.set(self._is_url, True)
+            spans.append(span)
+
+        return doc
+
+    def has_url(self, tokens):
+        return any(token._.get(self._is_url) for token in tokens)
+
+    def iter_url(self, tokens):
+        return [(i, t) for i, t in enumerate(tokens) if t._.get(self._is_url)]     
+
+class EmailDetector(object):
+    """spaCy v2.0 pipeline component for adding email address meta data to `Doc` objects."""
+    name='email_addr_detection'
+
+    def __init__(self, nlp, pattern_id='EmailAddrDetector', attrs=('has_email_addr', 'is_email_addr', 'email_addr')):
+        self._has_email_addr, self._is_email_addr, self._email_addr = attrs
+        self.matcher = Matcher(nlp.vocab)
+
+        # Add email address rule to matcher
+        self._email_addr_re = re.compile(email_expr, re.VERBOSE | re.I | re.UNICODE)
+        email_addr_mask = lambda text: bool(self._email_addr_re.match(text))
+        email_addr_flag = nlp.vocab.add_flag(email_addr_mask)
+        self.matcher.add('email_addr', None, [{email_addr_flag: True}])
+        
+        # Add attributes
+        Doc.set_extension(self._has_email_addr, getter=self.has_email_addr)
+        Doc.set_extension(self._email_addr, getter=self.iter_email_addr)
+        Span.set_extension(self._has_email_addr, getter=self.has_email_addr)
+        Span.set_extension(self._email_addr, getter=self.iter_email_addr)
+        Token.set_extension(self._is_email_addr, default=False)
+
+    def __call__(self, doc):
+        """Apply the pipeline component to a `Doc` object.
+
+        doc (Doc): The `Doc` returned by the previous pipeline component.
+        RETURNS (Doc): The modified `Doc` object.
+        """
+        matches = self.matcher(doc)
+        spans = []  # keep spans here to merge them later
+        for match_id, start, end in matches:
+            span = doc[start : end]
+            for token in span:
+                token._.set(self._is_email_addr, True)
+            spans.append(span)
+
+        return doc
+
+    def has_email_addr(self, tokens):
+        return any(token._.get(self._is_email_addr) for token in tokens)
+
+    def iter_email_addr(self, tokens):
+        return [(i, t) for i, t in enumerate(tokens) if t._.get(self._is_email_addr)]     
+
 
--- cyberspacy/expressions.py
+++ cyberspacy/expressions.py
@@ -1,3 +1,4 @@
+# Taken from Regular Expressions Cookbook 2nd edition by Levithan and Goyvaerts
 ipv4_expr = r"""
 (?:
 (?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.
@@ -6,3 +7,49 @@ ipv4_expr = r"""
 (?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])
 )
 """
+
+# Taken from Regular Expressions Cookbook 2nd edition by Levithan and Goyvaerts
+ipv6_expr = r"""
+(?:
+# Mixed
+ (?:
+  # Non-compressed
+  (?:[A-F0-9]{1,4}:){6}
+  # Compressed with at most 6 colons
+ |(?=(?:[A-F0-9]{0,4}:){0,6}
+     (?:[0-9]{1,3}\.){3}[0-9]{1,3}  # and 4 bytes
+     (?![:.\w]))                    # and anchored
+  # and at most 1 double colon
+  (?:(?:[0-9A-F]{1,4}:){0,5}|:)(?:(?::[0-9A-F]{1,4}){1,5}:|:)
+  # Compressed with 7 colons and 5 numbers
+ |::(?:[A-F0-9]{1,4}:){5}
+ )
+ # 255.255.255.
+ (?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}
+ # 255
+ (?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])
+|# Standard
+ (?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}
+|# Compressed with at most 7 colons
+ (?=(?:[A-F0-9]{0,4}:){0,7}[A-F0-9]{0,4}
+    (?![:.\w]))  # and anchored
+ # and at most 1 double colon
+ (?:(?:[0-9A-F]{1,4}:){1,7}|:)(?:(?::[0-9A-F]{1,4}){1,7}|:)
+ # Compressed with 8 colons
+|(?:[A-F0-9]{1,4}:){7}:|:(?::[A-F0-9]{1,4}){7}
+)
+"""
+
+email_expr = r"""[\w!#$%&'*+/=?`{|}~^-]+(?:\.[\w!#$%&'*+/=?`{|}~^-]+)*@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}"""
+
+url_expr = r"""
+(?:(?:(?:(?:
+  (?:ht|f)tp(?:s?)://|~/)
+  (?:[-;:&=+$,\w'.]+@)?
+  (?:[A-Za-z0-9.-:]+))
+  |(?:(?:www\.|[-;:&=+$,\w'.]+@)
+  (?:[A-Za-z0-9.-:]+))|
+  (?:(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})))
+  (?:[-\w~!$+|.,=/\?'#%*:&@;]*))
+"""
+
